afcd02ec4a6e4df0b94c8a2ece02dd5ff07dd0df
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-02-16 20:33:08 +0300

    Project start, database added

---
+++ identityAppProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace identityAppProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
