f60543b813266340759f477137a48a9dc3d5823b
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-02-17 20:54:09 +0300

    sign in added

---
+++ identityAppProject/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using identityAppProject.Models;
+using identityAppProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace identityAppProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser>userManager, SignInManager<IdentityUser>signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            loginViewModel.ReturnUrl = returnUrl ?? Url.Content("~/");
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, loginViewModel.RememberMe, lockoutOnFailure: true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(loginViewModel);
+                }
+            }
+            return View(loginViewModel);
+        }
+
+
+        public async Task<IActionResult> Register(string? returnUrl = null)
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            registerViewModel.ReturnUrl= returnUrl;
+            return View(registerViewModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel,string? returnUrl=null)
+        {
+            registerViewModel.ReturnUrl= returnUrl;
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { Email = registerViewModel.Email,UserName=registerViewModel.UserName };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user,isPersistent: false);
+
+                    return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError("Email", "User could not be created. Password not unique enough");
+
+            }
+            return View(registerViewModel);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
